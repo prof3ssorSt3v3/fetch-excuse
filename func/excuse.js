4aa81c2dd0982eb427d3802b9f0358bc47d44efc
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2022-06-21 22:03:49 -0400

    added node-fetch dependency

--- func/excuse.js
+++ func/excuse.js
@@ -1,10 +1,21 @@
 // /.netlify/functions/excuse
+const fetch = require('node-fetch')
+
+const API_ENDPOINT = 'https://theexcusegoose.com/generate/';
 
 exports.handler = async function (event, context) {
     //event - similar to express Request object
-    let url = 'https://theexcusegoose.com/generate/';
-    let response = await fetch(url);
-    
+    let response;
+    try{
+        response = await fetch(API_ENDPOINT);
+    }catch(err){
+        return {
+            statusCode: err.statusCode || 500,
+            body: JSON.stringify({
+                error: err.message
+            })
+        }
+    }
 
     return {
       "statusCode": response.status,
