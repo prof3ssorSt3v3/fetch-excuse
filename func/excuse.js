9251b21b83b909beac35e935ee0d789e4c39cb61
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2022-06-21 22:17:42 -0400

    update to excuse fetch

--- func/excuse.js
+++ func/excuse.js
@@ -1,13 +1,14 @@
-// /.netlify/functions/excuse
+// /.netlify/functions/excuse   
+// https://superlative-kataifi-3c0c26.netlify.app/api/excuse
 import fetch from 'node-fetch';
 
 const API_ENDPOINT = 'https://theexcusegoose.com/generate/';
 
 exports.handler = async function (event, context) {
-    //event - similar to express Request object
     let response;
     try{
         response = await fetch(API_ENDPOINT);
+        const body = await response.text();
     }catch(err){
         return {
             statusCode: err.statusCode || 500,
@@ -20,6 +21,6 @@ exports.handler = async function (event, context) {
     return {
       "statusCode": response.status,
       "headers": { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
-      "body": response.body,
+      "body": body,
     };
   };
