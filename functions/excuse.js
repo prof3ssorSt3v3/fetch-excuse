dea7d0fcf2105dbc4dd25ffef5e7312be64507e5
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2022-06-24 18:39:47 -0400

    add headers to request

--- functions/excuse.js
+++ functions/excuse.js
@@ -7,10 +7,19 @@ exports.handler = async function (event, context) {
     let why = 'my code is compiling';
     let what = 'waiting';
     let url = `https://function.xkcd-excuse.com/v1/excuse?who=${who}&why=${why}&what=${what}`;
-    url='https://jsonplaceholder.typicode.com/users';
+    
+    let headers = new fetch.Headers();
+    headers.append("Content-Type", "application/json");
+    headers.append('Access-Control-Allow-Origin', "*");
+
     let response, body;
+    const requestOptions = {
+        method: 'GET',
+        headers: headers,
+        // redirect: 'follow'
+    };
     try{
-        response = await fetch(url);
+        response = await fetch(url, requestOptions);
         body = await response.json();
         console.log(body);
 
